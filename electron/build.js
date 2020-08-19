bbeb5735de17efd891419104122eef2fd07f8c39
Thesam1798 <debris.alex@gmail.com>
2020-08-19 17:05:38 +0200

    Test <Tarvis> : Test des build

--- electron/build.js
+++ electron/build.js
@@ -23,9 +23,16 @@ builder.build({
         artifactName: '${productName}-setup-${version}.${ext}',
         copyright: 'Copyright © 2018-2020 Alexandre Debris',
         directories: {
-            buildResources: 'build',
+            buildResources: process.env.ELECTRON_BUILDER_DIR + '/electron/build',
             output: process.env.ELECTRON_BUILDER_DIR + '/output'
         },
+        files: [
+            process.env.ELECTRON_BUILDER_DIR + '/dist/**/*',
+            process.env.ELECTRON_BUILDER_DIR + '/electron/src/**/*',
+            process.env.ELECTRON_BUILDER_DIR + '/electron/index.js'
+        ],
+        compression: 'maximum',
+        asar: true,
         remoteBuild: false,
         win: {
             target: [
@@ -52,17 +59,7 @@ builder.build({
             synopsis: 'Modded Minecraft Launcher',
             description: 'Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.',
             category: 'Game'
-        },
-        compression: 'maximum',
-        files: [
-            process.env.ELECTRON_BUILDER_DIR + '/dist/**/*',
-            process.env.ELECTRON_BUILDER_DIR + '/electron/src/**/*',
-            process.env.ELECTRON_BUILDER_DIR + '/electron/index.js'
-        ],
-        extraResources: [
-            'libraries'
-        ],
-        asar: true
+        }
     }
 }).then(() => {
     console.log('Build complete!')
