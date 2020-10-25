f4becae466cfca2c70ecef0d04994d01ec9ed967
Thesam1798 <debris.alex@gmail.com>
2020-10-25 14:14:47 +0100

    Test <version>: Test de l'auto update

--- electron/build.js
+++ electron/build.js
@@ -19,8 +19,8 @@ builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
         appId: 'eclosialauncher',
-        productName: 'Eclosia Launcher',
-        artifactName: '${productName}-setup-${version}.${ext}',
+        productName: 'Eclosia-Launcher',
+        artifactName: 'Eclosia-Launcher-setup-${version}.${ext}',
         copyright: 'Copyright © 2018-2020 Alexandre Debris',
         directories: {
             buildResources: 'build',
